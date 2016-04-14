df10b13a61b53790d42fe3bd8b66e5043aa02974
chi <chi.zhang@xiaomi.com>
2016-04-14 10:39:52 +0800

    ex7 init

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -22,6 +22,25 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+tries = [0.01,0.03,0.1,0.3,1,3,10,30];
+
+min_err = -1;
+
+for C_t = tries
+  for sigma_t = tries
+    model = svmTrain(X, y, C_t, @(x1, x2) gaussianKernel(x1, x2, sigma_t), 1e-3, 20);
+    predictions = svmPredict(model, Xval);
+    err = mean(double(predictions ~= yval));
+    %printf("c: %f sigma: %f err: %f\n", C_t, sigma_t, err);
+    if min_err < 0 || err < min_err
+      
+      C = C_t;
+      sigma = sigma_t;
+      min_err = err;
+      %printf("MIN c: %f sigma: %f err: %f\n", C, sigma, err);
+    end
+  end
+end
 
 
 
