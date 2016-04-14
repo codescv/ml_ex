467800d7fa1d0e69a34d33204971cc0541198e65
chi <chi.zhang@xiaomi.com>
2016-04-14 11:44:51 +0800

    Kmeans

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -21,7 +21,11 @@ idx = zeros(size(X,1), 1);
 % Note: You can use a for-loop over the examples to compute this.
 %
 
-
+for i = 1:size(X,1)
+  d = repmat(X(i, :), K ,1) - centroids;
+  ds = sum(d .* d, 2);
+  [a, idx(i)] = min(ds);
+end
 
 
 
